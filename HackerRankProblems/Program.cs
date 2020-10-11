6b95720d01f1ae9676f186a3f9e99974ca09307f
Kirubel Tolosa <59670007+KirubelTolosa@users.noreply.github.com>
2020-10-11 15:36:14 -0400

    Decrypt message - solution added

--- HackerRankProblems/Program.cs
+++ HackerRankProblems/Program.cs
@@ -41,7 +41,82 @@ namespace test
             //Console.WriteLine(CountOccurences(str, 1000000000000).ToString());
             InsertionSort2(myArray);
         }
-        /*
+/*
+Decrypt Message
+An infamous gang of cyber criminals named “The Gray Cyber Mob”, which is behind many hacking attacks and drug trafficking, has recently become a target for the FBI. 
+After intercepting some of their messages, which looked like complete nonsense, the agency learned that they indeed encrypt their messages, and studied their method 
+of encryption.
+Their messages consist of lowercase latin letters only, and every word is encrypted separately as follows:
+Convert every letter to its ASCII value. Add 1 to the first letter, and then for every letter from the second one to the last one, add the value of the previous letter. 
+Subtract 26 from every letter until it is in the range of lowercase letters a-z in ASCII. Convert the values back to letters.
+
+For instance, to encrypt the word “crime”
+Decrypted message:	c	r	i	m	e
+Step 1:	99	114	105	109	101
+Step 2:	100	214	319	428	529
+Step 3:	100	110	111	116	113
+Encrypted message:	d	n	o	t	q
+The FBI needs an efficient method to decrypt messages. Write a function named decrypt(word) that receives a string that consists of small latin letters only,
+and returns the decrypted word.
+
+Explain your solution and analyze its time and space complexities.
+Examples:
+
+input:  word = "dnotq"
+output: "crime"
+
+input:  word = "flgxswdliefy"
+output: "encyclopedia"
+Since the function should be used on messages with many words, make sure the function is as efficient as possible in both time and space. 
+Explain the correctness of your function, and analyze its asymptotic runtime and space complexity.
+
+Note: Most programing languages have built-in methods of converting letters to ASCII values and vica versa. You may search the internet for the appropriate method.
+
+Constraints:
+[time limit] 5000ms
+[input] string word
+The ASCII value of every char is in the range of lowercase letters a-z.
+[output] string
+*/
+public static string Decrypt(string word)
+{
+      int charValue = 0;
+      int prevValue = 0;
+      StringBuilder decryptedWord = new StringBuilder();
+      if(word.Equals(word.ToLower()) == true)
+      {
+        for(int i=0; i< word.Length; i++)        
+        {
+          charValue = (int)(word[i]);
+          if(i == 0)
+          {                                           
+            while(charValue < 97)
+            {
+              charValue = charValue + 26;
+            }
+            decryptedWord.Append((char)(charValue - 1));    
+          }       
+          else
+          {            
+            charValue = charValue - prevValue;
+            while(charValue < 97)
+            {
+              charValue = charValue + 26;
+            }
+            decryptedWord.Append((char)(charValue));           
+          }          
+           prevValue = (int)(word[i]);
+        }
+      }  
+      else
+      {
+        return "Wrong Input!";
+      }
+      return decryptedWord.ToString();
+    }
+}
+	    
+/*
 Validate IP Address
 Validate an IP address (IPv4). An address is valid if and only if it is in the form "X.X.X.X", 
 where each X is a number from 0 to 255. For example, "12.34.5.6", "0.23.25.0", and 
